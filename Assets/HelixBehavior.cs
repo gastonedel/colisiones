4b3a2ac906ed4bd7f4edd26efcd327d5566fdea9
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-05-24 10:07:16 -0300

    Initial commit

---
+++ Assets/HelixBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixBehavior : MonoBehaviour {
+    public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.eulerAngles += new Vector3(0, rotationSpeed, 0);
+	}
+
+}
